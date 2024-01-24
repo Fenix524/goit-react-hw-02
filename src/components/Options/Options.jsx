fbfac9c30977b7ec48da24f4919ce2307f2fbc26
Herzanich Georgii <109143250+Fenix524@users.noreply.github.com>
2024-01-24 01:46:33 +0100

    Add dynamic refrash btn

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -2,24 +2,30 @@ import css from "./Options.module.css";
 import { BsEmojiSmile, BsEmojiNeutral, BsEmojiFrown } from "react-icons/bs";
 import { IoRefreshCircleOutline } from "react-icons/io5";
 
-const Options = ({ reactionController }) => {
+const Options = ({ reactionController, reactions: { good, neutral, bad } }) => {
   return (
     <div className={css.options}>
       <button className={css.btn} onClick={reactionController.updateGood}>
-        <BsEmojiSmile size={30}/>
+        <BsEmojiSmile size={30} />
         <p>Good</p>
       </button>
       <button className={css.btn} onClick={reactionController.updateNeutral}>
-        <BsEmojiNeutral size={30}/>
+        <BsEmojiNeutral size={30} />
         <p>Neutral</p>
       </button>
       <button className={css.btn} onClick={reactionController.updateBad}>
-        <BsEmojiFrown size={30}/>
+        <BsEmojiFrown size={30} />
         <p>Bad</p>
       </button>
-      <button className={css.btn} onClick={reactionController.clearAllReactions}>
-        <IoRefreshCircleOutline size={30}/>
-      </button>
+
+      {(good + neutral + bad) ? (
+        <button
+          className={css.btn}
+          onClick={reactionController.clearAllReactions}
+        >
+          <IoRefreshCircleOutline size={30} />
+        </button>
+      ) : null}
     </div>
   );
 };
